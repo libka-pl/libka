45c50689ddf2467e936363374259a62070459159
rysson <robert.kalinowski@sharkbits.com>
2022-03-14 21:41:13 +0100

    [site] Improve Site, add new methods, add base. Docs.

---
+++ doc/en/site.md
@@ -0,0 +1,152 @@
+Usage
+-----
+
+To use `requests` wrapper you need to create an instance of `Site`.
+
+### Mixin
+
+Simplest way to use `Site` is inherit from it.
+All methods from `Site` are available simply by `self`.
+
+```python
+from libka import Plugin, Site
+
+class MyPlugin(Site, Plugin):
+    ...
+```
+
+There are already prepared classes which join Addon/Plugin and Site benefits:
+`libka.SimpleAddon` and `libka.SimplePlugin`.
+The same, all methods from `Site` are available simply by `self`.
+
+```python
+from libka import SimplePlugin
+
+class MyPlugin(SimplePlugin):
+
+    def __init__(self):
+        super.__init__()
+        self.base = 'https://extra.site.net/api'
+
+    def home(self):
+        """Home folder. Main entry to plugin."""
+        # get JSON from https://extra.site.net/api/categories
+        data = self.jget('categories')
+        with self.directory() as kdir:
+            for cat in data:
+                kdir.menu(cat['title'], call(self.category, cid=cat['id']))
+
+    def category(cid):
+        """Category content."""
+        data = self.jget('http://another.site.net/categorty/{cid}',
+                         params={'quality': 'FHD'}, on_fail=[])
+        with self.directory() as kdir:
+            for movie in data:
+                kdir.play(movie['title'], call(self.play, cid=movie['id']),
+                          info={'duration': movie['duration'])
+```
+
+
+### Instance
+
+Another way to use `Site` is create instance (of more instances).
+
+```python
+from libka import Plugin, Site
+
+class MyPlugin(Plugin):
+
+    def __init__(self):
+        super.__init__()
+        self.site = Site(base='https://extra.site.net/api')
+        self.another = Site(base='http://another.site.net')
+        self.anysite = Site()
+        self.anysite.jpost('https://any.org/token', params={'user': 'me'},
+                           json={'token': 'abcde'})
+
+    def home(self):
+        """Home folder. Main entry to plugin."""
+        # get JSON from https://extra.site.net/api/categories
+        data = self.site.jget('categories')
+        with self.directory() as kdir:
+            for cat in data:
+                kdir.menu(cat['title'], call(self.category, cid=cat['id']))
+
+    def category(cid):
+        """Category content."""
+        # get JSON from http://another.site.net/categorty/<category-id>
+        data = self.another.jget('categorty/{cid}',
+                                 params={'quality': 'FHD'}, on_fail=[])
+        with self.directory() as kdir:
+            for movie in data:
+                kdir.play(movie['title'], call(self.play, cid=movie['id']),
+                          info={'duration': movie['duration'])
+```
+
+
+Details
+-------
+
+Base method to work with sites is `Site.request` (it returns `requests.Response`)
+but easier use is more specifics:
+
+| Method | `requests.Response` | JSON           | Text           |
+| ------ | ------------------- | -------------- | -------------- |
+| GET    | `Site.get`          | `Site.jget`    | `Site.txtget`  |
+| POST   | `Site.post`         | `Site.jpost`   | `Site.txtpost` |
+| PUT    | `Site.put`          | `Site.jput`    | —              |
+| PATCH  | `Site.patch`        | `Site.jpatch`  | —              |
+| DELETE | `Site.delete`       | `Site.jdelete` | —              |
+| HEAD   | —                   | —              | —              |
+
+HEAD method must be called by `Site.request('HEAD', ...)`.
+
+All `j*` methods return JSON directly. All `txt*` methods return response `text` directly.
+
+Each method has method uses the same arguments like `Site.request` except first (`meth=`),
+because HTTP method name is already in specific `Site` method.
+
+Second difference is argument `on_fail=` can contain JSON value for `j*` methods
+and string for `txt*` methods.
+
+
+### URL
+
+Full URL could be used in every method. When `Site` has set `base` property relative URL are also available.
+
+
+### `on_fail`
+
+Argument `on_fail=` in every request method could be:
+
+- missing, than default `Undefined` is used – exception will be raised on fail
+- callable function – `on_fail()` will be called and it result will be returned
+- any other value will be returned
+
+`Site.request` and specific methods like `Site.get` need return response object (or similar).  
+`j*` methods like `Site.jget` need return decoded JSON.  
+`txt*` methods like `Site.txtget` need return `str` text.
+
+```python
+from libka import SimplePlugin
+
+class MyPlugin(SimplePlugin):
+
+    def __init__(self):
+        super.__init__()
+        self.base = 'https://extra.site.net/api'
+        # even if fails empty string will be return, then `re` could be called
+        text = self.jget('https://good.site.net/', on_fail='')
+        for match in re.finditer(r'<a href="(?P<url>.*?)">(?P<title>.*?)</a>', text):
+            url, title = match.group('url', 'title')
+
+    def home(self):
+        """Home folder. Main entry to plugin."""
+        # even if fails empty list [] will be return, then `for in data` could work
+        data = self.jget('https://extra.site.net/api/categories', on_fail=[])
+        with self.directory() as kdir:
+            for cat in data:
+                kdir.menu(cat['title'], call(self.category, cid=cat['id']))
+```
+
+...
--- script.module.libka/lib/libka/site.py
+++ script.module.libka/lib/libka/site.py
@@ -1,3 +1,18 @@
+"""
+Access to sites. Wrapper for requests module.
+
+.. include:: ../../../doc/en/site.md
+
+
+See: https://docs.python-requests.org/en/latest/user/quickstart/
+"""
+
+from typing import (
+    TYPE_CHECKING,
+    Union, Any,
+)
+from functools import wraps
+from collections.abc import Mapping
 import requests
 import json as jsonlib
 from requests.exceptions import SSLError
@@ -11,35 +26,97 @@ except ImportError:
     except ImportError:
         JSONDecodeError = ValueError
 from certifi import where
-from collections.abc import Mapping
 from http.cookiejar import LWPCookieJar
 from .utils import encode_url, encode_params
+from .url import URL
 from .logs import log
 
 
-class UL3Response:
-    """urllib3 request response with API similar to requests response."""
+class Undefined:
+    """Helper. Value is undefined."""
+
+
+class URL3Response:
+    """Helper. `urllib3` request response with API similar to `requests` response."""
 
     def __init__(self, resp):
+        #: urllib3 response.
         self.resp = resp
+        #: Decoded unicode text. Lazy loaded.
         self._text = None
+        #: Decoded JSON. Lazy loaded.
         self._json = None
 
     @property
-    def constent(self):
+    @wraps(requests.Response.ok)
+    def ok(self):
+        """Returns True if status_code is less than 400, False if not."""
+        return self.resp.status < 400
+
+    @property
+    def status_code(self):
+        """Integer Code of responded HTTP Status, e.g. 404 or 200."""
+        return self.resp.status
+
+    @property
+    @wraps(requests.Response.content)
+    def content(self):
+        """Content of the response, in bytes."""
         return self.resp.data
 
     @property
+    @wraps(requests.Response.text)
     def text(self):
+        """Content of the response, in unicode."""
         if self._text is None:
             # TODO: detect html encoding
             self._text = self.resp.data.decode('utf-8')
         return self._text
 
-    def json(self):
+    @wraps(requests.Response.json)
+    def json(self, **kwargs):
+        """Returns the json-encoded content of a response, if any."""
         if self._json is None:
-            self._json = jsonlib.loads(self.text)
+            self._json = jsonlib.loads(self.text, **kwargs)
         return self._json
+
+    @property
+    def url(self):
+        """Final URL location of Response."""
+        return self.resp.geturl()
+
+    @property
+    def headers(self):
+        """Case-insensitive Dictionary of Response Headers."""
+        return self.resp.headers
+
+    @property
+    def cookies(self):
+        """A CookieJar of Cookies the server sent back."""
+        # TODO: implement
+        assert False, "Not implemented yet"
+
+
+def _resp_failed(on_fail):
+    """
+    Helper. Handle `on_fail` in value specific `Site` methods (like `Site.jget`). Must me called under `except`.
+
+    Parameters
+    ----------
+    on_fail : Undefined
+        Raise caught exception.
+    on_fail : callable
+        Call `on_fail` and returns result.
+    on_fail : any
+        Result to return.
+
+    In all cases except `Undefined` result is returned as JSON.
+    """
+    if on_fail is Undefined:
+        raise
+    if callable(on_fail):
+        return on_fail()
+    return on_fail
 
 
 class Site:
@@ -60,11 +137,13 @@ class Site:
     ...    def __init__(self):
     ...        super(MyAddon, self).__init__()
     ...        self.site = Site(base='URL')
+    ...        self.site.jget('https://very.good.url/')
 
-    Can be used as mixin too.
+    Can be used as mixin too like in `libka.SimplePlugin`.
     >>> class MyAddon(Site, Addon):
     ...    def __init__(self):
     ...        super(MyAddon, self).__init__(base='URL')
+    ...        self.jget('https://very.good.url/')
     """
 
     #: Default value. True if SSL should be verified
@@ -73,8 +152,9 @@ class Site:
     UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.30 Safari/537.36'
 
     def __init__(self, *args, **kwargs):
+        base = kwargs.pop('base', None)
         #: Base site URL.
-        self.base = kwargs.pop('base', None)
+        self.base = None if base is None else (URL(base) / '')
         #: Requests session.
         self._sess = None
         #: Path to cookie file or None.
@@ -83,9 +163,9 @@ class Site:
         self.verify_ssl = self.VERIFY_SSL
         #: User-Agent.
         self.ua = self.UA
-        #: Option to use urllib3 instead requests.
-        self.use_urllib3 = False
-        # --- call next contructors if Site is used as a mixin ---
+        #: Option to use request worker (ex. `urllib3` instead of default requests).
+        self.site_request_worker = None
+        # --- call next contractor if Site is used as a mixin ---
         super(Site, self).__init__(*args, **kwargs)
 
     @property
@@ -98,9 +178,9 @@ class Site:
                 self._sess.cookies = LWPCookieJar(self.cookiefile)
         return self._sess
 
-    def _request(self, meth, url, params=None, data=None, json=None, headers=None, cookies=None,
-                 verify_ssl=None, worker=None):
-        """Realize a network request."""
+    def _request(self, meth, url, *, params=None, data=None, json=None, headers=None,
+                 verify=None, worker=None, cache=None, **kwargs):
+        """Helper. Realize a network request."""
 
         def set_header(name, value):
             """Set default header, set only if not exist."""
@@ -109,12 +189,13 @@ class Site:
                 header_keys.add(key)
                 headers[name] = value
 
-        # xbmc.log('PLAYER.PL: getRequests(%r, data=%r, headers=%r, params=%r)'
-        #          % (url, data, headers, params), xbmc.LOGWARNING)
-        if verify_ssl is None:
-            verify_ssl = self.verify_ssl
-        if worker is None and self.use_urllib3:
-            worker = 'urllib3'
+        if verify is None:
+            verify = self.verify_ssl
+        if worker is None:
+            worker = self.site_request_worker
+        url = URL(url)
+        if not url.is_absolute():
+            url = self.base.join(url.path)
         if isinstance(params, Mapping):
             url, params = encode_url(url, params=params), None
 
@@ -124,12 +205,14 @@ class Site:
             set_header('Content-Type', 'application/json')
             set_header('Accept', 'application/json')
         set_header('Accept', '*/*')
-        set_header('User-Agent', self.ua)
+        if self.ua is not None:
+            set_header('User-Agent', self.ua)
+        # --- worker 'urllib3' ---
         if worker == 'urllib3':
             pool_kwargs = {}
-            if verify_ssl is False:
+            if verify is False:
                 pool_kwargs['cert_reqs'] = 'CERT_NONE'
-            elif verify_ssl is True:
+            elif verify is True:
                 pool_kwargs['ca_certs'] = where()
             http = urllib3.PoolManager(**pool_kwargs)
             if json is not None:
@@ -139,52 +222,158 @@ class Site:
                 set_header('Conttent-Type', 'application/x-www-form-urlencoded')
             elif data is not None and not isinstance(data, bytes):
                 data = data.encode('utf-8')
-            resp = http.request(meth, url, headers=headers, body=data)
-            resp = UL3Response(resp)
+            fields = kwargs.pop('fields', None)
+            resp = http.request(meth, str(url), headers=headers, body=data, fields=fields)
+            resp = URL3Response(resp)
+        # --- default worker ---
         else:
-            log('req(%s, %r, params=%r, data=%r, json=%r, headers=%r, cookies=%r)' % (meth, url, params, data, json, headers, cookies))  # XXX
-            resp = self.sess.request(meth, url, params=params, data=data, json=json,
-                                     headers=headers, cookies=cookies, verify=verify_ssl)
+            log(f'libka.req({meth}, {url!r}, params={params!r}, data={data!r}, json={json!r},'
+                f' headers={headers!r}, {kwargs})')  # XXX
+            resp = self.sess.request(meth, str(url), params=params, data=data, json=json, headers=headers,
+                                     verify=verify, **kwargs)
         return resp
 
-    def request(self, meth, url, **kwargs):
-        """Make a network request. If SSLError try to call self.ssl_dialog()."""
+    def request(self, meth: str, url: Union[URL, str], *,
+                on_fail: Any = Undefined, on_fail_wrapper: str = None, **kwargs):
+        """
+        Make a network request. On SSLError try to call self.ssl_dialog().
+
+        Parameters
+        ----------
+        meth : str
+            Uppercase request method like GET or POST.
+        url : str or libka.url.URL
+            Request URL.
+        params : dict or list of tuples or bytes
+            Query string parameters.
+        data : dict or list of tuples or bytes or file-like object
+            Data to send in the body of the Request.
+        json : any
+            A JSON serializable Python object to send in the body of the Request.
+        headers : dict
+            Dictionary of HTTP Headers to send.
+        verify : bool
+            Check certificates if true (default).
+        worker : str
+            Request worker. Default is `None` for requests`.
+            Use `'urllib3'` for use `urllib3` (not fully supported).
+        cache: bool or int
+            Number of seconds to cache request result. If `True` cache for 24h.
+        on_fail: any or callable
+            Value (even `None`) to return on failed or callback to call on failed.
+
+            - `Undefined` – exception is raised (default)
+            - `callable` – calls `on_fail` and returns it result
+            - any – result to return
+
+        Returns
+        -------
+        Request response object, see `requests.Response`.
+
+        Details
+        -------
+        Rest of keyword arguments (like `allow_redirects`) are redirect directly to `requests.request`.
+
+        See: https://docs.python-requests.org/en/latest/api/#requests.request
+        """
+        def failed():
+            if on_fail is Undefined:
+                raise
+            if callable(on_fail):
+                return on_fail()
+            return on_fail
+
         try:
             return self._request(meth, url, **kwargs)
         except SSLError:
             # requests SSLError
             ssl_dialog = getattr(self, 'ssl_dialog', None)
             if ssl_dialog is None:
-                raise
+                return failed()
             ssl_dialog()
         except MaxRetryError as exc:
             if not isinstance(exc.reason, SSLError3):
-                raise
+                return failed()
             # urllib3 SSLError
             ssl_dialog = getattr(self, 'ssl_dialog', None)
             if ssl_dialog is None:
-                raise
+                return failed()
             ssl_dialog()
-        return self._request(meth, url, **kwargs)
+        except Exception:
+            return failed()
+        try:
+            return self._request(meth, url, **kwargs)
+        except Exception:
+            return failed()
 
     def get(self, url, **kwargs):
-        """GET request. Keywoard arguments line in _request()."""
+        """GET request. For arguments see `Site.request()`."""
         return self.request('GET', url, **kwargs)
 
     def post(self, url, **kwargs):
-        """POST request. Keywoard arguments line in _request()."""
+        """POST request. For arguments see `Site.request()`."""
         return self.request('POST', url, **kwargs)
 
-    def jget(self, url, **kwargs):
-        """GET request returns JSON. Keyword arguments line in _request()."""
-        return self.get(url, **kwargs).json()
+    def put(self, url, **kwargs):
+        """PUT request. For arguments see `Site.request()`."""
+        return self.request('PUT', url, **kwargs)
 
-    def jpost(self, url, **kwargs):
-        """POST request returns JSON. Keyword arguments line in _request()."""
-        # return self.post(url, **kwargs).json()
-        log('jpost(%r, %r)' % (url, kwargs))  # XXX
-        resp = self.post(url, **kwargs)
-        return resp.json()
+    def patch(self, url, **kwargs):
+        """PATCH request. For arguments see `Site.request()`."""
+        return self.request('PATCH', url, **kwargs)
+
+    def delete(self, url, **kwargs):
+        """DELETE request. For arguments see `Site.request()`."""
+        return self.request('DELETE', url, **kwargs)
+
+    def jget(self, url, *, on_fail: Any = Undefined, **kwargs):
+        """GET request, returns JSON. For arguments see `Site.request()`."""
+        try:
+            return self.get(url, **kwargs).json()
+        except Exception:
+            return _resp_failed(on_fail)
+
+    def jpost(self, url, *, on_fail: Any = Undefined, **kwargs):
+        """POST request, returns JSON. For arguments see `Site.request()`."""
+        try:
+            return self.post(url, **kwargs).json()
+        except Exception:
+            return _resp_failed(on_fail)
+
+    def jput(self, url, *, on_fail: Any = Undefined, **kwargs):
+        """PUT request, returns JSON. For arguments see `Site.request()`."""
+        try:
+            return self.put(url, **kwargs).json()
+        except Exception:
+            return _resp_failed(on_fail)
+
+    def jpatch(self, url, *, on_fail: Any = Undefined, **kwargs):
+        """PUT request, returns JSON. For arguments see `Site.request()`."""
+        try:
+            return self.patch(url, **kwargs).json()
+        except Exception:
+            return _resp_failed(on_fail)
+
+    def jdelete(self, url, *, on_fail: Any = Undefined, **kwargs):
+        """DELETE request, returns JSON. For arguments see `Site.request()`."""
+        try:
+            return self.delete(url, **kwargs).json()
+        except Exception:
+            return _resp_failed(on_fail)
+
+    def txtget(self, url, *, on_fail: Any = Undefined, **kwargs):
+        """GET request, returns text. For arguments see `Site.request()`."""
+        try:
+            return self.get(url, **kwargs).text
+        except Exception:
+            return _resp_failed(on_fail)
+
+    def txtpost(self, url, *, on_fail: Any = Undefined, **kwargs):
+        """POST request, returns text. For arguments see `Site.request()`."""
+        try:
+            return self.post(url, **kwargs).text
+        except Exception:
+            return _resp_failed(on_fail)
 
 
 # def getRequests3(url, data=None, headers=None, params=None):
